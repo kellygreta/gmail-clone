95a4405e0f4c9eafd191c76049aa8b63346cac8a
Greta Vigano <gretavigano@Air-di-Greta.homenet.telecomitalia.it>
2023-03-03 16:03:12 +0100

    prime modifiche

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="flex bg-red-200 text-center text-2xl">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+	const handleSubmit = (e) => {
+		// getting the action url
+		const ACTION_URL = e.target.action;
+
+		// get the form fields data and convert it to URLSearchParams
+		const formData = new FormData(e.target);
+		const data = new URLSearchParams();
+		for (let field of formData) {
+			const [key, value] = field;
+			data.append(key, value);
+		}
+
+		// check the form's method and send the fetch accordingly
+		if (e.target.method.toLowerCase() == 'get') fetch(`${ACTION_URL}?${data}`);
+		else {
+			fetch(ACTION_URL, {
+				method: 'POST',
+				body: data
+			});
+		}
+	};
+</script>
+
+<h1>Gmail</h1>
+
+<input name="cerca-email" placeholder="Cerca nella posta" />
+
+<div>in arrivo, speciali, bozze, eliminati</div>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<input name="destinatario" placeholder="Destinatario" />
+	<input name="oggetto" placeholder="Oggetto" />
+	<input name="email-body" placeholder="Scrivi..." />
+	<input type="submit" value="Invia" />
+</form>
+
+<!-- https://jsonplaceholder.typicode.com/users -->
