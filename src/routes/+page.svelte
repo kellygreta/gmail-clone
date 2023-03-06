9d67933949c51e31a6ccd3ac9e3f668f277af6a2
Greta Vigano <gretavigano@Air-di-Greta.homenet.telecomitalia.it>
2023-03-06 14:41:41 +0100

    aggiunte classi tailwind e TODO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	//TODO prendere i sender da https://jsonplaceholder.typicode.com/users
+	//TODO prendere i sender in modo casuale da https://jsonplaceholder.typicode.com/users
 
 	//TODO Le mail devono essere salvate, indipendentemente da refresh o stop dell’applicazione.
 	//TODO Le mail possono essere ordinabili per data, dalla più recente a quella più vecchia e viceversa
@@ -7,7 +7,6 @@
 	function addSuccess() {
 		var success = document.getElementById('successImport');
 		success.style.display = 'block';
-		success.innerHTML = '<p> Email inviata! </p>';
 	}
 
 	function searchMail() {
@@ -31,51 +30,67 @@
 				}
 			}
 		}
-
-		var myInput = document.getElementById('searchMail');
-
-		myInput.onfocus = function () {
-			document.getElementById('resultMail').style.display = 'block';
-		};
-
-		myInput.onblur = function () {
-			document.getElementById('resultMail').style.display = 'none';
-		};
-
-		function addEmail() {
-			emails = window.localStorage.getItem('emails');
-
-			if (emails === null) {
-				emails = [];
-			} else {
-				emails = JSON.parse(emails);
-			}
-
-			//TODO da rivedere
-			//var attachments = [];
-
-			var email = {
-				//TODO add sender
-				//sender:
-
-				recipient: document.getElementById('recipient').value,
-				subject: document.getElementById('subject').value,
-				body: document.getElementById('email-body').value,
-
-				//TODO stato da rivedere
-				stato: ''
-
-				//TODO add attachments
-				//attachments:
-			};
-
-			//TODO controllo validità email
-
-			emails.push(email);
-
-			window.localStorage.setItem('emails', JSON.stringify(emails));
-		}
 	}
+
+	var myInput = document.getElementById('searchMail');
+
+	myInput.onfocus = function () {
+		document.getElementById('resultMail').style.display = 'block';
+	};
+
+	myInput.onblur = function () {
+		document.getElementById('resultMail').style.display = 'none';
+	};
+
+	function addEmail() {
+		emails = window.localStorage.getItem('emails');
+
+		if (emails === null) {
+			emails = [];
+		} else {
+			emails = JSON.parse(emails);
+		}
+
+		//TODO da rivedere come allegare file
+		//var attachments = [];
+
+		var email = {
+			//TODO add sender
+			//sender:
+
+			recipient: document.getElementById('recipient').value,
+			subject: document.getElementById('subject').value,
+			body: document.getElementById('email-body').value,
+
+			//TODO stato da rivedere
+			stato: ''
+
+			//TODO add attachments
+			//attachments:
+		};
+
+		//TODO controllo validità email
+
+		emails.push(email);
+
+		window.localStorage.setItem('emails', JSON.stringify(emails));
+	}
+
+	/*function getSender() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error('Qualcosa è andato storto');
+			})
+			.then((json) => {
+				console.log(json);
+				//TODO prendere utente sender con id Math.floor(Math.random() * 10) + 1;
+				//TODO aggiungere sender al local storage (?)
+			})
+			.catch((error) => console.log(error));
+	}*/
 </script>
 
 <h1>Gmail</h1>
@@ -83,9 +98,16 @@
 <!--TODO Le mail potranno essere divise tra: “in arrivo”, “bozze”, “speciali” -->
 <div>in arrivo, speciali, bozze, eliminati</div>
 
-<div id="successImport" class="alert alert-success" role="alert" style="display: none;" />
+<div
+	id="successImport"
+	class="relative rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700"
+	role="alert"
+	style="display: none;"
+>
+	<span class="block sm:inline">Email inviata con successo!</span>
+</div>
 
-<div class="container mt-3">
+<div class="container m-3">
 	<form>
 		<input
 			name="cerca"
@@ -98,8 +120,8 @@
 	</form>
 </div>
 
-<div class="container mt-3">
-	<table class="table-bordered table">
+<div class="container m-3">
+	<table class="table-auto">
 		<thead>
 			<tr>
 				<th>Mittente</th>
@@ -111,9 +133,9 @@
 	</table>
 </div>
 
-<div class="container mt-3">
+<div class="container m-3">
 	<form on:submit={addEmail}>
-		<div class="col mb-3">
+		<div class="m-3 grid-cols-1">
 			A <input
 				name="recipient"
 				id="recipient"
@@ -126,7 +148,7 @@
 
 		<!-- TODO add CC -->
 
-		<div class="col mb-3">
+		<div class="m-3 grid-cols-1">
 			<input
 				name="subject"
 				id="subject"
@@ -137,7 +159,7 @@
 			/>
 		</div>
 
-		<div class="col mb-3">
+		<div class="m-3 grid-cols-1">
 			<input
 				name="email-body"
 				id="email-body"
@@ -150,8 +172,10 @@
 
 		<!-- TODO add allegati, caricabili sia con il classico selettore di file che con un sistema di drag and drop  -->
 
-		<div class="col mb-3">
-			<button class="btn btn-primary">Invia</button>
+		<div class="m-3 grid-cols-1">
+			<button class="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
+				Invia
+			</button>
 		</div>
 	</form>
 </div>
